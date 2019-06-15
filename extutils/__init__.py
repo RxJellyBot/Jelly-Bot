78d0618465841048ac185c4744d82fcadad68a75
RaenonX JELLYCAT <raenonx0710@gmail.com>
2019-06-15 03:01:37 -0700

    import bug fix

--- extutils/__init__.py
+++ extutils/__init__.py
@@ -1,6 +1,4 @@
 # noinspection PyUnresolvedReferences
 from .utils import cast_keep_none, is_empty_string
 # noinspection PyUnresolvedReferences
-from line_sticker.main import LineStickerManager, LineStickerType
-# noinspection PyUnresolvedReferences
 from .timing import exec_timing
