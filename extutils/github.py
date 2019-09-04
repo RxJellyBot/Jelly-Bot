10cf7e1c7d359af251f86b1720e578082f818626
RaenonX <raenonx0710@gmail.com>
2019-09-04 17:25:16 -0500

    Added build status on about page

---
+++ extutils/github.py
@@ -0,0 +1,29 @@
+import requests
+from dotmap import DotMap
+
+
+class GitHubWrapper:
+    API_URL = "https://api.github.com"
+
+    def __init__(self):
+        self._cache_deployments = {}
+
+    # noinspection PyMethodMayBeStatic
+    def get_latest_deployment(self, environment):
+        if environment not in self._cache_deployments:
+            response = requests.get(f"{GitHubWrapper.API_URL}/repos/RaenonX/Jelly-Bot-API/deployments", {
+                "environment": environment
+            })
+
+            list_ = response.json()
+
+            if list_:
+                self._cache_deployments[environment] = list_
+                return DotMap(list_[0])
+            else:
+                return None
+        else:
+            return DotMap(self._cache_deployments[environment][0])
+
+
+_inst = GitHubWrapper()
