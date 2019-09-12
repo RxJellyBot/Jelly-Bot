a2164a051525d28ba8c59351ffa7ad95b8690364
RaenonX <raenonx0710@gmail.com>
2019-09-11 23:05:33 -0500

    Line webhook test async

--- external/line/handler/text.py
+++ external/line/handler/text.py
@@ -9,7 +9,7 @@ def handle_text(event):
     print("Line event triggered. [TextMessage]")
     # FIXME: [HP] Implement total handle and return all at once somewhere
     # FIXME: Add webhook/bot links on home page
-    # FIXME: [MP] https://discordpy.readthedocs.io/en/latest/logging.html
+    # FIXME: [MP] https://discordpy.readthedocs.io/en/latest/logging.html / LOGGER = logging.getLogger('linebot')
     # FIXME: Discord handler
 
     LineApiWrapper.reply_text(event.reply_token, event.message.text)
