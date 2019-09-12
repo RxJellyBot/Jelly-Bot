6271fcff16c72410bc9fc980f5e3c62f09087123
RaenonX <raenonx0710@gmail.com>
2019-09-11 22:31:36 -0500

    Line webhook hotfix (debug)

--- webhook/line/handler/default.py
+++ webhook/line/handler/default.py
@@ -4,4 +4,4 @@ from ..base import line_handler
 @line_handler.default()
 def handle_default(event):
     # FIXME: Handle Line event default
-    pass
+    print("Line event triggered. [Default]")
--- webhook/line/handler/text.py
+++ webhook/line/handler/text.py
@@ -6,7 +6,10 @@ from ..base import line_handler
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_text(event):
+    print("Line event triggered. [TextMessage]")
     # FIXME: [HP] Implement total handle and return all at once somewhere
     # FIXME: Add webhook link on home page
+    # FIXME: [MP] Create logger class
+    # FIXME: Discord handler
 
     LineApiWrapper.reply_text(event.reply_token, event.message.text)
