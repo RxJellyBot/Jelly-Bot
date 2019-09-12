f18668d9180111cac46141ce6b6f12c38b84ecac
RaenonX <raenonx0710@gmail.com>
2019-09-12 01:30:19 -0500

    Discord bot test

--- external/line/handler/message/text.py
+++ external/line/handler/message/text.py
@@ -2,6 +2,6 @@ def handle_text(event, destination):
     # FIXME: [HP] Implement total handle and return all at once somewhere (Integrate text message first)
     # FIXME: Add webhook/bot links on home page
     # FIXME: [MP] https://discordpy.readthedocs.io/en/latest/logging.html / LOGGER = logging.getLogger('linebot')
-    # FIXME: Discord handler
+    # FIXME: Discord handler - https://github.com/nick411077/repo_bot/blob/master/cogs/help.py
 
     print(event.message.text)
