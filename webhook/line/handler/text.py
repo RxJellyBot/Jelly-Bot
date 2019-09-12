ae41bff899579dc42b2bb5ba0fdd92d9d7812faf
RaenonX <raenonx0710@gmail.com>
2019-09-11 22:12:44 -0500

    Line webhook hotfix

--- webhook/line/handler/text.py
+++ webhook/line/handler/text.py
@@ -6,6 +6,7 @@ from ..base import line_handler
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_text(event):
-    # FIXME: Implement total handle and return all at once somewhere
+    # FIXME: [HP] Implement total handle and return all at once somewhere
+    # FIXME: Add webhook link on home page
 
     LineApiWrapper.reply_text(event.reply_token, event.message.text)
