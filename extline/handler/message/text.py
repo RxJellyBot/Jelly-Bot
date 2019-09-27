1f8f8974ae3c1f65fb5efb5e9a3f7ad6053ebcaa
RaenonX <raenonx0710@gmail.com>
2019-09-27 00:26:46 -0500

    hotfix import

--- extline/handler/message/text.py
+++ extline/handler/message/text.py
@@ -7,10 +7,11 @@ from msghandle.models import EventObjectFactory
 
 
 def handle_text(request, event, destination):
-
     # FIXME: Discord handler
     #  REF 1: https://github.com/nick411077/repo_bot/blob/master/cogs/help.py
     #  REF 2: https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html
+
+    # FIXME: [LP] Temporary activate gettext, actual depends on personal config
 
     activate('zh-tw')
     handled_events = handle_main(EventObjectFactory.from_line(event)).to_platform(Platform.LINE)
