29eaf41d0f8b310e42f34d5307ffb18690a949ef
RaenonX <raenonx0710@gmail.com>
2019-11-01 09:08:06 -0500

    Temp solution for keeping beta up

--- extutils/spamping.py
+++ extutils/spamping.py
@@ -1,4 +1,6 @@
-import asyncio
+import time
+from threading import Thread
+
 import requests
 
 from JellyBot.systemconfig import HostUrl
@@ -9,17 +11,17 @@ __all__ = ["activate_ping_spam"]
 logger = LoggerSkeleton("sys.pingspam", logger_name_env="PING_SPAM")
 
 
-async def _spam_ping_(cd_sec: int, retry_sec: int = 60):
+def _spam_ping_(cd_sec: int, retry_sec: int = 60):
     # Prevent from sleep
     while True:
         try:
             requests.get(HostUrl)
             logger.logger.info(f"Ping spammed to {HostUrl}.")
-            await asyncio.sleep(cd_sec)
+            time.sleep(cd_sec)
         except (requests.exceptions.ConnectionError, ConnectionRefusedError):
             logger.logger.warning(f"Ping failed to spam on {HostUrl}. ConnectionError. Retry in {retry_sec} seconds.")
-            await asyncio.sleep(retry_sec)
+            time.sleep(retry_sec)
 
 
 def activate_ping_spam(cd_sec: int, retry_sec: int = 60):
-    asyncio.ensure_future(_spam_ping_(cd_sec, retry_sec))
+    Thread(target=_spam_ping_, args=(cd_sec, retry_sec)).start()
