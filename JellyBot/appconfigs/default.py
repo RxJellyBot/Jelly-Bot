9562dd39ad302f7516ec0b52b6ea8cb70fe43b55
RaenonX <raenonx0710@gmail.com>
2019-11-01 03:05:56 -0500

    Test Parallel

--- JellyBot/appconfigs/default.py
+++ JellyBot/appconfigs/default.py
@@ -1,16 +1,8 @@
-from multiprocessing import Process
-
 from django.apps import AppConfig
-
-from extdiscord import run_server
-
-
-def discord_main():
-    run_server()
 
 
 class JellyBotAppConfig(AppConfig):
     name = "JellyBot"
 
     def ready(self):
-        Process(target=discord_main).start()
+        pass
---
+++ staticfiles/js/utils/xhr.js
@@ -0,0 +1,26 @@
+// from https://stackoverflow.com/a/48969580
+
+// noinspection JSUnusedGlobalSymbols
+function makeXHRPromise(method, url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
