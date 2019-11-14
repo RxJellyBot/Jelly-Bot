f24b4afd54494663aad16443fa3f6ecaa37c27f1
RaenonX <raenonx0710@gmail.com>
2019-11-14 17:47:48 -0600

    Hotfix image upload on LINE

--- mongodb/factory/stats.py
+++ mongodb/factory/stats.py
@@ -47,7 +47,7 @@ class MessageRecordStatisticsManager(BaseCollection):
             message_type: MessageType, message_content: Any) -> MessageRecordResult:
         entry, outcome, ex = self.insert_one_data(
             ChannelOid=channel_oid, UserRootOid=user_root_oid, MessageType=message_type,
-            MessageContent=message_content[:Database.MessageStats.MaxContentCharacter])
+            MessageContent=repr(message_content)[:Database.MessageStats.MaxContentCharacter])
 
         return MessageRecordResult(outcome, entry, ex)
 
