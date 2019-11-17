eea82e899e1077d75ed39814d37a6befbac0ffd6
RaenonX <raenonx0710@gmail.com>
2019-11-17 17:03:58 -0600

    Testing discord bot conflict preventer

--- extdiscord/utils/cnflprvt.py
+++ extdiscord/utils/cnflprvt.py
@@ -2,28 +2,29 @@ from typing import Optional
 
 from discord import Guild, Status
 
-__all__ = ["record_current_id", "prioritized_bot_exists"]
+__all__ = ["initialize", "prioritized_bot_exists"]
 
 print("Discord Bot Conflict Preventer is active. "
       "Disable this by deleting the usage of `log_bot_presence` and `prioritized_bot_exists` in `extdiscord.core` "
       "if you forked the code repository.")
 
-
-CURRENT_BOT_ID = -1
 LOCAL_BOT_ID = 623262302510252032
 BETA_BOT_ID = 621537611026137118
 STABLE_BOT_ID = 621539717841944587
 
-ids_to_check = [LOCAL_BOT_ID, BETA_BOT_ID, STABLE_BOT_ID]
+
+id_list_checked = False
+
+id_to_check = [LOCAL_BOT_ID, BETA_BOT_ID, STABLE_BOT_ID]
 
 
 def prioritized_bot_exists(dc_guild: Optional[Guild]):
-    if CURRENT_BOT_ID == -1:
-        print("Discord Bot Conflict Preventer not initialized. Did you execute `record_current_id` once?")
+    if not id_list_checked:
+        print("Discord Bot Conflict Preventer not initialized. Did you execute `initialize()` once?")
         return False
 
     if dc_guild is not None:
-        for id_ in get_id_list_to_check():
+        for id_ in id_to_check:
             bot = dc_guild.get_member(id_)
             if bot and bot.status == Status.online:
                 return True
@@ -31,10 +32,12 @@ def prioritized_bot_exists(dc_guild: Optional[Guild]):
     return False
 
 
-def get_id_list_to_check():
-    return [id_ for id_ in ids_to_check if id_ != CURRENT_BOT_ID]
+def initialize(id_: int):
+    global id_to_check, id_list_checked
 
+    try:
+        id_to_check = id_to_check[:id_to_check.index(id_)]
+    except ValueError:
+        print(f"ID {id_} is not in the bot ID list.")
 
-def record_current_id(id_: int):
-    global CURRENT_BOT_ID
-    CURRENT_BOT_ID = id_
+    id_list_checked = True
