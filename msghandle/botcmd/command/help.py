457d88ed302657989fdb24b61ebafffe9b8956db
RaenonX <raenonx0710@gmail.com>
2019-11-17 17:20:22 -0600

    Added help command

---
+++ msghandle/botcmd/command/help.py
@@ -0,0 +1,30 @@
+from gettext import gettext as _
+
+from flags import BotFeature
+from msghandle.models import TextMessageEventObject, HandledMessageEventText
+
+from ._base_ import CommandNode
+
+cmd = CommandNode(
+    codes=["help"], order_idx=1000, name=_("Help"),
+    description=_("See the documentation or bot related information."))
+
+
+@cmd.command_function(feature_flag=BotFeature.TXT_BOT_HELP)
+def help_text(e: TextMessageEventObject):
+    return [
+        HandledMessageEventText(
+            content=_("LINE BOT:\n"
+                      "Beta: http://rnnx.cc/LineBeta\n"
+                      "Stable: http://rnnx.cc/LineStable\n"
+                      "\n"
+                      "Discord:\n"
+                      "Beta: http://rnnx.cc/DiscordBeta\n"
+                      "Stable: http://rnnx.cc/DiscordStable\n"
+                      "\n"
+                      "Website:\n"
+                      "Beta: http://rnnx.cc/WebsiteBeta\n"
+                      "Stable: http://rnnx.cc/WebsiteStable"),
+            bypass_multiline_check=True
+        )
+    ]
