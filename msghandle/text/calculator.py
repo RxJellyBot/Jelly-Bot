cfa7a2af0e9e5aa568fd90aa1c88b125eaf77065
RaenonX <raenonx0710@gmail.com>
2019-12-04 16:16:40 -0600

    [FIX] Hotfix Calculator - Fail Silently

--- msghandle/text/calculator.py
+++ msghandle/text/calculator.py
@@ -43,7 +43,5 @@ def process_calculator(e: TextMessageEventObject) -> List[HandledMessageEvent]:
             logger.logger.debug(
                 f"Exception occurred for text message calculator. Expr: {e.expr} / Base Exception: {e.base_exc}")
             return []
-        except SyntaxError as e:
-            logger.logger.debug(
-                f"Exception occurred for text message calculator. Expr: {e.expr} / Base Exception: {e.base_exc}")
+        except SyntaxError:
             return []
