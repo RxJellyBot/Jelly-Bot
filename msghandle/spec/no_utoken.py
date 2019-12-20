84f3b19ab79f029d9e2a63f71d861e369c8df7d8
RaenonX <raenonx0710@gmail.com>
2019-12-20 00:52:34 -0600

    [IMP] Optimize the speed of message handling

--- msghandle/spec/no_utoken.py
+++ msghandle/spec/no_utoken.py
@@ -4,13 +4,13 @@ from django.utils.translation import gettext_lazy as _
 from ttldict import TTLOrderedDict
 
 from JellyBot.systemconfig import System, HostUrl
-from msghandle.models import TextMessageEventObject, HandledMessageEvent, HandledMessageEventText
+from msghandle.models import MessageEventObject, HandledMessageEvent, HandledMessageEventText
 
 
 _sent_cache_ = TTLOrderedDict(System.NoUserTokenNotificationSeconds)
 
 
-def handle_no_user_token(e: TextMessageEventObject) -> List[HandledMessageEvent]:
+def handle_no_user_token(e: MessageEventObject) -> List[HandledMessageEvent]:
     if e.channel_oid not in _sent_cache_:
         _sent_cache_[e.channel_oid] = True
 
