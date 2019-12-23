40e66e1706775dbded8647edcf29a175d5fe7e5b
RaenonX JELLYCAT <raenonx0710@gmail.com>
2019-12-22 19:52:15 -0600

    [FIX] #104

--- mongodb/factory/user.py
+++ mongodb/factory/user.py
@@ -202,7 +202,7 @@ class RootUserManager(BaseCollection):
 
                 if onplat_data:
                     return UserNameQuery(
-                        user_id=root_oid, user_name=onplat_data.get_name(channel_data, mark_x_on_not_found=True))
+                        user_id=root_oid, user_name=onplat_data.get_name(channel_data))
                 else:
                     MailSender.send_email(
                         f"OnPlatOid {onplatoid} was found to bind with the root data of {root_oid}, but no "
