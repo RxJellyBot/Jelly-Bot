939afa8ee7a9c92b7fb93288faff7cfe5139a585
RaenonX <raenonx0710@gmail.com>
2020-02-15 03:32:35 -0600

    [FIX] #172 - Revert the removal of Walgreens
    Actually it's a desync between the Walgreens' online system and the store's system
    
    This reverts commit b79976cc870bca1b588d3253471255356fd2b31f.

--- extutils/maskfinder/main.py
+++ extutils/maskfinder/main.py
@@ -4,10 +4,14 @@ from .response import Response
 from .result import Result
 
 PRODUCT_TARGET = ("53134238", "Anti-Viral Face Mask - 10ct - Up&Up™")
+PRODUCT_WALGREENS1 = ("40000153109", "Walgreens Face Mask, Cone Style")
+PRODUCT_WALGREENS2 = ("40000384673", "Walgreens Earloop-Style Face Mask")
 
 
 def get_results(zip_code, target_range_mi, silent_fail=True) -> List[Result]:
     results = []
     results.extend(Response.get_target(zip_code, PRODUCT_TARGET, target_range_mi, silent_fail))
+    results.extend(Response.get_walgreens(zip_code, PRODUCT_WALGREENS1, silent_fail))
+    results.extend(Response.get_walgreens(zip_code, PRODUCT_WALGREENS2, silent_fail))
 
     return sorted(results, key=lambda x: x.distance)
--- extutils/maskfinder/result.py
+++ extutils/maskfinder/result.py
@@ -16,9 +16,3 @@ class Result:
             return f"{self.name} ({self.time})"
         else:
             return self.name
-
-    def __str__(self):
-        s = f"{self.product_name}\n"
-        s += f"<{self.amount:^7d}> {self.name} (Time: {self.time})\n"
-        s += f"{self.distance} mi - {self.address}"
-        return s
