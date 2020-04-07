b02ef1618e56a205edacb7eaa64b4960b091c764
raenonx0710@gmail.com <raenonx0710@gmail.com>
2020-04-07 17:45:18 -0500

    [ADD] #200 - `bot.RemoteControl` [IMP] Reduce dependability of `RemoteControl`
    Signed-off-by: raenonx0710@gmail.com <raenonx0710@gmail.com>

--- tests/bot/utils/calculator.py
+++ tests/bot/utils/calculator.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from bot.utils import calculate_expression
 
 
@@ -12,10 +13,10 @@ class TestBotCalculator(TestCase):
 
         result = result[0]
 
-        self.assertEquals(expr, result.calc_expr)
-        self.assertEquals("10", result.calc_result)
-        self.assertFalse(result.latex_available)
-        self.assertFalse(result.has_evaluated)
+        self.assertEquals(expr, result.calc_expr, "Calculation expression not match.")
+        self.assertEquals("10", result.calc_result, "Calculation result not match.")
+        self.assertFalse(result.latex_available, "LaTeX should not be available.")
+        self.assertFalse(result.has_evaluated, "Expression should not be evaluated.")
 
     def test_calculate_float(self):
         expr = "5.5+5"
@@ -26,10 +27,10 @@ class TestBotCalculator(TestCase):
 
         result = result[0]
 
-        self.assertEquals(expr, result.calc_expr)
-        self.assertEquals("10.5", result.calc_result)
-        self.assertFalse(result.latex_available)
-        self.assertFalse(result.has_evaluated)
+        self.assertEquals(expr, result.calc_expr, "Calculation expression not match.")
+        self.assertEquals("10.5", result.calc_result, "Calculation result not match.")
+        self.assertFalse(result.latex_available, "LaTeX should not be available.")
+        self.assertFalse(result.has_evaluated, "Expression should not be evaluated.")
 
     def test_calculate_rational(self):
         expr = "7/2 + 1"
@@ -40,11 +41,11 @@ class TestBotCalculator(TestCase):
 
         result = result[0]
 
-        self.assertEquals(expr, result.calc_expr)
-        self.assertEquals("9/2", result.calc_result)
-        self.assertTrue(result.latex_available)
-        self.assertTrue(result.has_evaluated)
-        self.assertEquals(4.5, result.evaluated)
+        self.assertEquals(expr, result.calc_expr, "Calculation expression not match.")
+        self.assertEquals("9/2", result.calc_result, "Calculation result not match.")
+        self.assertTrue(result.latex_available, "LaTeX should be available.")
+        self.assertTrue(result.has_evaluated, "Expression should be evaluated.")
+        self.assertEquals(4.5, result.evaluated, "Evaluated result not match.")
 
     def test_solve_equation(self):
         expr = "solve(x-1)"
@@ -55,10 +56,10 @@ class TestBotCalculator(TestCase):
 
         result = result[0]
 
-        self.assertEquals(expr, result.calc_expr)
-        self.assertEquals("[1]", result.calc_result)
-        self.assertTrue(result.latex_available)
-        self.assertFalse(result.has_evaluated)
+        self.assertEquals(expr, result.calc_expr, "Calculation expression not match.")
+        self.assertEquals("[1]", result.calc_result, "Calculation result not match.")
+        self.assertTrue(result.latex_available, "LaTeX should be available.")
+        self.assertFalse(result.has_evaluated, "Expression should not be evaluated.")
 
     def test_multiline(self):
         expr = "a = 7\na + 1"
@@ -69,17 +70,17 @@ class TestBotCalculator(TestCase):
 
         result = result[0]
 
-        self.assertEquals("a + 1", result.calc_expr)
-        self.assertEquals("8", result.calc_result)
-        self.assertFalse(result.latex_available)
-        self.assertFalse(result.has_evaluated)
+        self.assertEquals("a + 1", result.calc_expr, "Calculation expression not match.")
+        self.assertEquals("8", result.calc_result, "Calculation result not match.")
+        self.assertFalse(result.latex_available, "LaTeX should not be available.")
+        self.assertFalse(result.has_evaluated, "Expression should not be evaluated.")
 
     def test_malformed(self):
         expr = "=_="
         result = calculate_expression(expr)
 
         if result:
-            self.fail("Got a calculation result")
+            self.fail(f"Got a calculation result.\n{result[0].to_json()}")
 
     def test_force_output_error(self):
         expr = "5+"
@@ -90,6 +91,6 @@ class TestBotCalculator(TestCase):
 
         result = result[0]
 
-        self.assertEquals(expr, result.calc_expr)
-        self.assertFalse(result.latex_available)
-        self.assertFalse(result.has_evaluated)
+        self.assertEquals(expr, result.calc_expr, "Calculation expression not match.")
+        self.assertFalse(result.latex_available, "LaTeX should not be available.")
+        self.assertFalse(result.has_evaluated, "Expression should not be evaluated.")
