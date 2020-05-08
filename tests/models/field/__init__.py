3a9b866a58507e3e9de8af565439771f3520dcd2
raenonx0710@gmail.com <raenonx0710@gmail.com>
2020-05-08 09:07:53 -0500

    [IMP] #244 - `__init__` file for `models.fields`
    Signed-off-by: raenonx0710@gmail.com <raenonx0710@gmail.com>

---
+++ tests/models/field/__init__.py
@@ -0,0 +1,7 @@
+from .array import (
+    TestArrayFieldExtra, TestArrayFieldProperty, TestArrayFieldValueDefault,
+    TestArrayFieldValueAllowNone, TestArrayFieldValueNoAutoCast, TestArrayFieldValueLengthLimited
+)
+from .bool import TestBoolField
+from .color import TestColorField
+from .datetime import TestDatetimeField
--- tests/models/fields/__init__.py
+++
@@ -1,4 +0,0 @@
-from .array import TestArrayField
-from .bool import TestBoolField
-from .color import TestColorField
-from .datetime import TestDatetimeField
