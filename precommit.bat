7fca7f40c6cdf3abab13b0b7e663198b7ad2cdb3
raenonx0710@gmail.com <raenonx0710@gmail.com>
2020-06-20 20:52:26 -0500

    [IMP] Precommit bat file
    Signed-off-by: raenonx0710@gmail.com <raenonx0710@gmail.com>

--- precommit.bat
+++ precommit.bat
@@ -1,4 +1,21 @@
+@ECHO OFF
+
+ECHO Checking Flake8 Error...
 flake8 . --config .flake8-error || ECHO ERROR && PAUSE && EXIT /b
+
+ECHO Checking Flake8 style...
 flake8 . --config .flake8-style || ECHO ERROR && PAUSE && EXIT /b
-pylint rxtoolbox || ECHO ERROR && PAUSE && EXIT /b
-py -m unittest tests || ECHO ERROR && PAUSE && EXIT /b
+
+ECHO Checking pylint...
+pylint bot doc extdiscord extline || ECHO ERROR && PAUSE && EXIT /b
+
+ECHO Running tests
+py manage.py test tests || ECHO ERROR && PAUSE && EXIT /b
+
+ECHO.
+ECHO.
+ECHO.
+ECHO =============================================
+ECHO ============== GREEN TO COMMIT ==============
+ECHO =============================================
+ECHO.
