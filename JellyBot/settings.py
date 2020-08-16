8186528d749afd444ee2699c14d06ccbbd0baaf8
raenonx0710@gmail.com <raenonx0710@gmail.com>
2020-08-15 19:05:20 -0500

    [FIX] Inconsistent quotation marks in Django settings
    Signed-off-by: raenonx0710@gmail.com <raenonx0710@gmail.com>

--- JellyBot/settings.py
+++ JellyBot/settings.py
@@ -16,14 +16,14 @@ from django.utils.translation import gettext_lazy as _
 
 # --- Main
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 if SECRET_KEY is None:
-    print('Django SECRET_KEY undefined.')
+    print("Django SECRET_KEY undefined.")
     sys.exit(1)
 
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
-PRODUCTION = bool(int(os.environ.get('PRODUCTION', 1)))
+PRODUCTION = bool(int(os.environ.get("PRODUCTION", 1)))
 
 # --- Application definition
 
@@ -32,69 +32,69 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 ALLOWED_HOSTS = [
     # Local IP addresses
-    'testserver',
-    'localhost',
-    '127.0.0.1',
-    '192.168.50.33',
+    "testserver",
+    "localhost",
+    "127.0.0.1",
+    "192.168.50.33",
     # URL Domain of the bot
-    'bot.raenonx.cc',
+    "bot.raenonx.cc",
 ]
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'sanitizer',
-    'JellyBot'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "sanitizer",
+    "JellyBot"
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'JellyBot.components.middleware.RootUserIDInsertMiddleware',
-    'JellyBot.components.middleware.APIStatisticsCollector',
-    'JellyBot.components.middleware.TimezoneActivator',
-    'JellyBot.components.middleware.TranslationActivator'
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "JellyBot.components.middleware.RootUserIDInsertMiddleware",
+    "JellyBot.components.middleware.APIStatisticsCollector",
+    "JellyBot.components.middleware.TimezoneActivator",
+    "JellyBot.components.middleware.TranslationActivator"
 ]
 
-ROOT_URLCONF = 'JellyBot.urls'
+ROOT_URLCONF = "JellyBot.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.template.context_processors.i18n',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.template.context_processors.i18n",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'JellyBot.wsgi.application'
+WSGI_APPLICATION = "JellyBot.wsgi.application"
 
 # --- Database
 # --- https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -103,34 +103,34 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # --- Internationalization
 # --- https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'zh-tw'
+LANGUAGE_CODE = "zh-tw"
 
 LANGUAGES = (
-    ('en-us', _('English')),
-    ('zh-tw', _('Traditional Chinese')),
+    ("en-us", _("English")),
+    ("zh-tw", _("Traditional Chinese")),
 )
 
 LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'locale'),
+    os.path.join(BASE_DIR, "locale"),
 )
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -141,7 +141,7 @@ USE_TZ = True
 # --- Static files (CSS, JavaScript, Images)
 # --- https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
@@ -153,7 +153,7 @@ LOGIN_URL = reverse_lazy("account.login")
 
 # --- On-Error handling
 
-ADMINS = [('RaenonX JELLYCAT', os.environ.get("EMAIL_ACCOUNT"))]
+ADMINS = [("RaenonX JELLYCAT", os.environ.get("EMAIL_ACCOUNT"))]
 
 # --- Email
 
@@ -161,8 +161,8 @@ if os.environ.get("EMAIL_ACCOUNT") is None:
     sys.exit("EMAIL_ACCOUNT not set in environment variable.")
 if os.environ.get("EMAIL_PASSWORD") is None:
     sys.exit("EMAIL_PASSWORD not set in environment variable.")
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_ACCOUNT")
@@ -170,7 +170,7 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 # --- Sanitizer
 
-SANITIZER_ALLOWED_TAGS = ['div', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span',
-                          'p', 'pre', 'code', 'br', 'img', 'ul', 'li']
-SANITIZER_ALLOWED_ATTRIBUTES = ['class', 'id', 'role', 'data-toggle', 'href', 'aria-labelledby', 'src']
+SANITIZER_ALLOWED_TAGS = ["div", "a", "h1", "h2", "h3", "h4", "h5", "h6", "span",
+                          "p", "pre", "code", "br", "img", "ul", "li"]
+SANITIZER_ALLOWED_ATTRIBUTES = ["class", "id", "role", "data-toggle", "href", "aria-labelledby", "src"]
 SANITIZER_ALLOWED_STYLES = []
--- JellyBot/settings_production.py
+++ JellyBot/settings_production.py
@@ -5,7 +5,7 @@ from JellyBot.settings import *  # noqa
 # --- SECURITY
 
 # Cross-site load in frame
-X_FRAME_OPTIONS = 'DENY'
+X_FRAME_OPTIONS = "DENY"
 
 # For SecurityMiddleware
 SECURE_BROWSER_XSS_FILTER = True
@@ -13,25 +13,25 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # --- CSRF
 
-CSRF_COOKIE_DOMAIN = 'raenonx.cc'
-CSRF_TRUSTED_ORIGINS = ['bot.raenonx.cc']
+CSRF_COOKIE_DOMAIN = "raenonx.cc"
+CSRF_TRUSTED_ORIGINS = ["bot.raenonx.cc"]
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.template.context_processors.i18n',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.template.context_processors.i18n",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
-            'loaders': [
-                ('django.template.loaders.cached.Loader', [
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader'
+            "loaders": [
+                ("django.template.loaders.cached.Loader", [
+                    "django.template.loaders.filesystem.Loader",
+                    "django.template.loaders.app_directories.Loader"
                 ]),
             ],
         },
@@ -44,26 +44,26 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # --- Logging
 
-LOGGING_ROOT = '../logs/Jelly-Bot/Application'
-LOGGING_FILE_ROOT = os.path.join(LOGGING_ROOT, 'logs')
-LOGGING_FILE_ERROR = os.path.join(LOGGING_FILE_ROOT, 'logs-severe.log')
+LOGGING_ROOT = "../logs/Jelly-Bot/Application"
+LOGGING_FILE_ROOT = os.path.join(LOGGING_ROOT, "logs")
+LOGGING_FILE_ERROR = os.path.join(LOGGING_FILE_ROOT, "logs-severe.log")
 
 # To see the log which level is < 30 (WARNING), `DEBUG` must set to 1.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'null': {
-            'class': 'logging.NullHandler',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "null": {
+            "class": "logging.NullHandler",
         },
     },
-    'loggers': {
-        'django.security.DisallowedHost': {
-            'handlers': ['null'],
-            'propagate': False,
+    "loggers": {
+        "django.security.DisallowedHost": {
+            "handlers": ["null"],
+            "propagate": False,
         },
     },
 }
 
-sys.stdout = open(os.path.join(LOGGING_ROOT, 'app.log'), "w+")
-sys.stderr = open(os.path.join(LOGGING_ROOT, 'app-error.log'), "w+")
+sys.stdout = open(os.path.join(LOGGING_ROOT, "app.log"), "w+")
+sys.stderr = open(os.path.join(LOGGING_ROOT, "app-error.log"), "w+")
