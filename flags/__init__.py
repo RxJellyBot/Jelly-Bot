c9c79d9e9940c7c69c3c63c99171de04525b298f
RaenonX <raenonx0710@gmail.com>
2020-09-06 21:23:43 -0500

    [IMP] Code refactor
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- flags/__init__.py
+++ flags/__init__.py
@@ -1,18 +1,10 @@
 """Main module for various defined flags."""
 # noinspection PyUnresolvedReferences
+from .apicmd import APICommand
+# noinspection PyUnresolvedReferences
 from .artype import AutoReplyContentType
 # noinspection PyUnresolvedReferences
-from .perm import ProfilePermission, ProfilePermissionDefault, PermissionLevel
-# noinspection PyUnresolvedReferences
-from .platforms import Platform
-# noinspection PyUnresolvedReferences
-from .execode import Execode, ExecodeCollationFailedReason, ExecodeCompletionOutcome
-# noinspection PyUnresolvedReferences
-from .apiact import APICommand
-# noinspection PyUnresolvedReferences
-from .result import ModelValidityCheckResult
-# noinspection PyUnresolvedReferences
-from .msgtype import MessageType
+from .botfeature import BotFeature
 # noinspection PyUnresolvedReferences
 from .chtype import ChannelType
 # noinspection PyUnresolvedReferences
@@ -20,8 +12,16 @@ from .cmdscope import CommandScopeCollection, CommandScope
 # noinspection PyUnresolvedReferences
 from .exctnt import ExtraContentType
 # noinspection PyUnresolvedReferences
-from .weberr import WebsiteError
+from .execode import Execode, ExecodeCollationFailedReason, ExecodeCompletionOutcome
 # noinspection PyUnresolvedReferences
 from .imgtype import ImageContentType
 # noinspection PyUnresolvedReferences
-from .botfeature import BotFeature
+from .msgtype import MessageType
+# noinspection PyUnresolvedReferences
+from .perm import ProfilePermission, ProfilePermissionDefault, PermissionLevel
+# noinspection PyUnresolvedReferences
+from .platforms import Platform
+# noinspection PyUnresolvedReferences
+from .result import ModelValidityCheckResult
+# noinspection PyUnresolvedReferences
+from .weberr import WebsiteError
--- models/stats/__init__.py
+++ models/stats/__init__.py
@@ -1,7 +1,7 @@
 """Implementations of the data/result models related to stats."""
 from .base import DailyResult, HourlyResult
 from .bot import BotFeatureUsageResult, BotFeatureHourlyAvgResult, BotFeaturePerUserUsageResult
-from .model import APIStatisticModel, MessageRecordModel, BotFeatureUsageModel
+from .main import APIStatisticModel, MessageRecordModel, BotFeatureUsageModel
 from .msg import (
     MemberMessageCountEntry, MemberMessageCountResult, HourlyIntervalAverageMessageResult, DailyMessageResult,
     MemberMessageByCategoryEntry, MemberMessageByCategoryResult, MemberDailyMessageResult, MeanMessageResultGenerator,
